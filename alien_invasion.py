85a83bdb2a5151934521b924a3f4037b0bb432ab
Madeleine <m.monica6889@gmail.com>
2020-08-02 20:30:14 +0430

    first commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,30 +1,41 @@
 import sys 
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvension:
 	"""Overaal class to manage game assets and behavoir"""
 	def __init__ (self):
 		"""Initialized the game, and create game resourses"""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((1200,800))
-		pygame.display.set_caption("Alien Invension")
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
+		pygame.display.set_caption("Alien Invension") 
 
-		# set the background color
-		self.bg_color = (100,100,100) 
+		self.ship = Ship(self)
 
 	def run_game (self):
 	   """Start the main loop for the game"""
 	   while True:
 	   	# watch for keyboard and mouse events.
-	   	for event in pygame.event.get():
+	   	self._check_events()
+	   	self._update_screen()
+
+	def _check_events (self):
+	   """respond to keypresses and mouse events"""
+	   for event in pygame.event.get():
 	   	   if event.type == pygame.QUIT:
 	   	      sys.exit()
-	   	#redraw the screen during each pass through the loop
-	   	self.screen.fill(self.bg_color)      
+	   	      
+	def _update_screen (self):
+	   #update images on screen and flip to the new screen
+	   self.screen.fill(self.settings.bg_color)
+	   self.ship.blitme()      
 
-	   	#make the most recently drwan screen visible.
-	   	pygame.display.flip()
+	   #make the most recently drwan screen visible.
+	   pygame.display.flip()
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion """
+	def __init__(self):
+		"""Initialized the gams's setting"""
+		# Screen setting
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (255,255,255)
+
+		
+		
