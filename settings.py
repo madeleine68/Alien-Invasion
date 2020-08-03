c67bb693d05acc9e78fc0f20c97111674341a794
Madeleine <m.monica6889@gmail.com>
2020-08-03 13:09:47 +0430

    adding movement to the ship

--- settings.py
+++ settings.py
@@ -3,9 +3,13 @@ class Settings:
 	def __init__(self):
 		"""Initialized the gams's setting"""
 		# Screen setting
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 800
+		self.screen_height = 500
 		self.bg_color = (255,255,250)
+
+		# Ship setting
+		self.ship_speed = 1.5
+
 
 		
 		
